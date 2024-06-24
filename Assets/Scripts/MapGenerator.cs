c73e75d6420e8ac50606d3007c3f76e75eab0554
Mohammed Ammar Al Hamadani <mohammedamaar165@gmail.com>
2024-06-24 20:41:59 +0200

    We a methode that positions blocks prefabs.

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -24,10 +24,39 @@ private int[,] mapLayout = {
     {1, 0, 0, 0, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1},
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
-    private void Start()
+  private void Start()
     {
-      
+        GenerateMap();
     }
 
+    void GenerateMap()
+    {
+       
+        float groundWidth = mapLayout.GetLength(0) * spacing;
+        float groundHeight = mapLayout.GetLength(1) * spacing;
 
+   
+        GameObject ground = Instantiate(groundPrefab, new Vector3(groundWidth / 2 - spacing / 2, -0.1f, groundHeight / 2 - spacing / 2), Quaternion.identity);
+        ground.transform.localScale = new Vector3(groundWidth, 1, groundHeight);
+
+      
+        for (int i = 0; i < mapLayout.GetLength(0); i++)
+        {
+            for (int j = 0; j < mapLayout.GetLength(1); j++)
+            {
+                int prefabIndex = mapLayout[i, j];
+                if (prefabIndex >= 0 && prefabIndex < prefabTypes.Length)
+                {
+                    Vector3 position = new Vector3(i * spacing, 0, j * spacing);
+                    Instantiate(prefabTypes[prefabIndex], position, Quaternion.identity);
+                }
+            }
+        }
+
+  
+    }
+
+   
 }
+
+
